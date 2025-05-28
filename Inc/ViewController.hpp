95b4017479fdbd6cc92ad4967912021b16c912ae
Daria Lekhtereva <darialekhtereva@gmail.com>
2025-05-28 16:58:05 +0300

    add view and controller

---
+++ Inc/ViewController.hpp
@@ -0,0 +1,36 @@
+#pragma once
+#include <memory>
+#include <string>
+#include "Model.hpp"
+
+struct View {
+    public:
+
+    virtual void Init(size_t width, size_t height, const std::string& wname) = 0;
+
+    virtual void Stop() = 0;
+
+    virtual void DrawField(const Model::Field& field) = 0;
+
+    virtual ~View = default;
+};
+
+struct Controller {
+    public:
+
+    struct MouseState{
+
+    enum ClickType { None, Left, Right } Click;
+
+    bool ClickUpdated;
+
+    int X
+    int Y
+    };
+
+    virtual MouseState GetMouseState() = 0;
+
+    virtual bool ShutDownPanding() = 0;
+
+    virtual ~Controller = default;
+};
